327b9bb5d00d3c38b940d6903ea9f1ca6c547854
filippoteodoro <filippo.teodoro@gmail.com>
2026-03-03 10:19:35 +0100

    Switch font to Geom

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,10 @@
 import type { Metadata } from 'next'
-import { Saira } from 'next/font/google'
+import { Geom } from 'next/font/google'
 import { GoogleAnalytics } from '@next/third-parties/google'
 import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 
-const saira = Saira({ subsets: ['latin'], weight: ['300', '400', '500', '600'] })
+const geom = Geom({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Filippo Teodoro',
@@ -14,7 +14,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={saira.className}>{children}</body>
+      <body className={geom.className}>{children}</body>
       <GoogleAnalytics gaId="G-GMFJT73TES" />
       <Analytics />
     </html>
