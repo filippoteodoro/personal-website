c40f65a5c1380b60670412a93e352ff8c5e41f1b
filippoteodoro <filippo.teodoro@gmail.com>
2026-03-03 11:39:17 +0100

    Switch font to Courier Prime

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,10 @@
 import type { Metadata } from 'next'
-import { Cutive } from 'next/font/google'
+import { Courier_Prime } from 'next/font/google'
 import { GoogleAnalytics } from '@next/third-parties/google'
 import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 
-const cutive = Cutive({ subsets: ['latin'], weight: '400' })
+const courierPrime = Courier_Prime({ subsets: ['latin'], weight: ['400', '700'] })
 
 export const metadata: Metadata = {
   title: 'Filippo Teodoro',
@@ -14,7 +14,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={cutive.className}>{children}</body>
+      <body className={courierPrime.className}>{children}</body>
       <GoogleAnalytics gaId="G-GMFJT73TES" />
       <Analytics />
     </html>
